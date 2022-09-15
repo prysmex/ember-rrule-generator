d72fdf084a8fd95e994cf3f39349f21102ecc949
betocantu93 <betocantu93@gmail.com>
2022-09-15 18:17:54 -0500

    fix: dates

--- addons/ember-rrule-generator/src/components/containers/end/on-date.ts
+++ addons/ember-rrule-generator/src/components/containers/end/on-date.ts
@@ -1,3 +1,17 @@
 import BaseContainerComponent from '../base-container';
+import { DATE_TIME_FORMAT } from 'ember-rrule-generator/utils/constants';
+import dayjs from 'dayjs';
 
-export default class ContainersEndOnDateComponent extends BaseContainerComponent {}
+export default class ContainersEndOnDateComponent extends BaseContainerComponent {
+  dateFormat = DATE_TIME_FORMAT;
+  handleDateChange(inputDate: Date | undefined) {
+    const editedEvent = {
+      target: {
+        value: dayjs(inputDate).format(DATE_TIME_FORMAT),
+        name: this.args.name,
+      },
+    };
+
+    this.args.handleChange(editedEvent);
+  }
+}
--- addons/ember-rrule-generator/src/components/containers/start/on-date.ts
+++ addons/ember-rrule-generator/src/components/containers/start/on-date.ts
@@ -1,3 +1,18 @@
 import BaseContainerComponent from '../base-container';
+import { DATE_TIME_FORMAT } from 'ember-rrule-generator/utils/constants';
+import dayjs from 'dayjs';
 
-export default class ContainersStartOnDateComponent extends BaseContainerComponent {}
+export default class ContainersStartOnDateComponent extends BaseContainerComponent {
+  dateFormat = DATE_TIME_FORMAT;
+
+  handleDateChange(inputDate: Date | undefined) {
+    const editedEvent = {
+      target: {
+        value: dayjs(inputDate).format(DATE_TIME_FORMAT),
+        name: this.args.name,
+      },
+    };
+
+    this.args.handleChange(editedEvent);
+  }
+}
